9ccec1a7476e8869f83e08fc68a3cfbe82d1c8a2
Lll05 <102873003+Lll05@users.noreply.github.com>
2022-09-03 18:22:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,7 +31,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ouUBF6AEu6qHlKYYABLDPssmZCn8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "zjVglIuC2Xo4CsraXWEio1iP7sZJ37IQEES9lD3sgc4yOU",
+      useTemplateId: "YcxPK4iKdH0RYkqM-FvO_tKcHqArHOxD2QWHpWOXczE",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
